e13e624215102c773e4666514b8aa3170e5aff91
Татьяна Николаева <tanya8621@mail.ru>
2022-09-29 09:39:18 +0300

    8

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,5 +5,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_p'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,8 @@ from .models import Post, Group
 
 def index(request):
     title = 'Это главная страница проекта Yatube'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = posts = Post.objects.all()[:10]
     context = {
-        # В словарь можно передать переменную
         'title': title,
         'posts': posts,
     }
@@ -14,15 +13,8 @@ def index(request):
 
 
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
     context = {
         'group': group,
         'posts': posts,
